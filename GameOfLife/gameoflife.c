abf3da705b44994cd1ffb2d37d801da8fac5cd8a
root <root@localhost.localdomain>
2018-08-01 02:17:22 +0200

    ITS ALIVE

--- GameOfLife/gameoflife.c
+++ GameOfLife/gameoflife.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void    ft_putchar(char c)
 {
@@ -17,9 +18,26 @@ void    ft_print_matrix(char matrix[10][11])
     {
         while (j <= 10)
         {
-            ft_putchar(' ');
-            ft_putchar(matrix[i][j]);
-            j++;
+            if (matrix[i][j] == 'd')
+            {
+                matrix[i][j] = '.';
+                ft_putchar(' ');
+                ft_putchar(matrix[i][j]);
+                j++;
+            }
+            else if (matrix[i][j] == 'c')
+            {
+                matrix[i][j] = 'o';
+                ft_putchar(' ');
+                ft_putchar(matrix[i][j]);
+                j++;
+            }
+            else
+            {
+                ft_putchar(' ');
+                ft_putchar(matrix[i][j]);
+                j++;
+            }
         }
         j = 0;
         i++;
@@ -37,7 +55,7 @@ void    ft_matrix(char matrix[10][11])
     {
         while (j <= 9)
         {
-            matrix[i][j] = 'x';
+            matrix[i][j] = '.';
             j++;
         }
         matrix[i][j] = '\n';
@@ -49,39 +67,63 @@ void    ft_matrix(char matrix[10][11])
 
 void    ft_add_life(char matrix[10][11])
 {
-    matrix[1][1] = 'o';
-    matrix[1][2] = 'o';
     matrix[2][2] = 'o';
-    matrix[2][1] = 'o';
-    matrix[2][3] = 'o';
+    matrix[3][2] = 'o';
+    matrix[4][2] = 'o';
+    matrix[4][1] = 'o';
+    matrix[3][0] = 'o';
+//    matrix[3][4] = 'o';
 }
 
 int    ft_count_live(int x, int y, char matrix[10][11])
 {
-    int     i;
-    int     j;
+    int     istart;
+    int     iend;
+    int     jstart;
+    int     jtmp;
+    int     jend;
     int     nlive;
 
-    j = x - 1;
-    i = y - 1;
     nlive = 0;
-    while (i <= (y + 2))
+    if ((x - 1) < 0)
     {
-        while (j <= (x + 2))
+        jstart = x;
+        jtmp = x;
+    }
+    else
+    {
+        jstart = x - 1;
+        jtmp = x - 1;
+    }
+    if ((x + 1) == '\n')
+        jend = x;
+    else
+        jend = x + 1;
+    if ((y - 1) < 0)
+        istart = y;
+    else
+        istart = y - 1;
+    if ((y + 1) == '\n')
+        iend = y;
+    else
+        iend = y + 1;
+    while (istart <= iend)
+    {
+        while (jstart <= jend)
         {
-            if (matrix[i][j] == 'o')
+            if (matrix[istart][jstart] == 'o' || matrix[istart][jstart] == 'd')
             {
                 nlive++;
-                j++;
+                jstart++;
             }
             else
-                j++;
+                jstart++;
         }
-        j = x - 1;
-        i++;
+        jstart = jtmp;
+        istart++;
     }
     nlive = nlive - 1;
-    printf("nlive = %d\n", nlive);
+//    printf("nlive = %d\n", nlive);
     return (nlive);
 }
 
@@ -90,6 +132,7 @@ void    ft_rules(char matrix[10][11])
 {
     int i;
     int j;
+    int xsituation;
 
     i = 0;
     j = 0;
@@ -107,25 +150,48 @@ void    ft_rules(char matrix[10][11])
                 else
                     j++;
             }
+            else if (matrix[i][j] == '.')
+            {
+                xsituation = ft_count_live(j, i, matrix) + 1;
+                if (xsituation == 3)
+                {
+                    matrix[i][j] = 'c';
+                    j++;
+                }
+                else
+                    j++;
+            }
             else
                 j++;
         }
         j = 0;
         i++;
     }
-
 }
 
 int     main(void)
 {
+    int     a;
     char    matrix[10][11];
-
+    
+    a = 1;
+    system("clear");
     ft_matrix(matrix);
     ft_putchar('\n');
+    sleep(1);
+    system("clear");
     ft_add_life(matrix);
-    ft_print_matrix(matrix);
-    ft_putchar('\n');
-    ft_rules(matrix);
-    ft_print_matrix(matrix);
+    while (a = 1)
+    {
+        ft_print_matrix(matrix);
+        ft_putchar('\n');
+        sleep(1);
+        system("clear");
+        ft_rules(matrix);
+        ft_print_matrix(matrix);
+        sleep(1);
+        system("clear");
+    }
+    
     return (0);
 }
